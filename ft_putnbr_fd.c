33ccbf60fb69c19c645ceefd8ebeec226694304a
jviknander <jviknander@gmail.com>
2021-11-02 18:35:27 +0000

    :flushed:

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -15,18 +15,19 @@
 void	ft_putnbr_fd(int nb, int fd)
 {
 	if (nb == -2147483648)
-		ft_putstr_fd("-214748364", fd);
+		ft_putchar_fd("-", fd);
+		ft_putnbr_fd("214748364", fd);
 	else 
 	{
 		if (nb < 0)
 		{
 			ft_putchar_fd('-', fd);
-			ft_putchar_fd(nb * -1, fd);
+			ft_putnbr_fd(nb * -1, fd);
 		}
 		if (nb >= 10)
 		{
-			ft_putchar_fd(nb / 10, fd);
-			ft_putchar_fd(nb % 10, fd);
+			ft_putnbr_fd(nb / 10, fd);
+			ft_putnbr_fd(nb % 10, fd);
 		}
 		else
 			ft_putchar_fd('0', fd);
