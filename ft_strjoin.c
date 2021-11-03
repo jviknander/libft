c0ea3e1a3ed69b509e4b121bae95516bfc120afe
jviknander <jviknander@gmail.com>
2021-11-03 20:49:39 +0000

    correct :angry:

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -14,6 +14,57 @@
 
 char *ft_strjoin(char const *s1, char const *s2)
 {
+	char	*str;
+	int	size;
+
+	size = ft_strlcat(s1) + ft_strlcat(s2);
+	if (!s1)
+		str = s2;
+	if (!s2)
+		str = s1;
+	str = malloc(size + 1)
+	if (!str)
+		return (NULL);
+
+	
+
 	ft_strlcat
-	return (news)
+	return (str)
 }
+
+
+/*
+
+[___s1____][___s2____]
+[_______str__________] SIZE
+
+if !s1 ou !s2
+[0][_______s2 s1_____]
+[_______str__________] SIZE
+str = s2
+
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int	size;
+	int i,
+	int j;
+	j = 0;
+	size = ft_strlcat(s1) + ft_strlcat(s2);
+	str = malloc(sizeof(char) * size + 1)
+	if (!str)
+		return (NULL);
+	while (s1[i])
+		str[i] = s1++;
+	while (s2)
+	stri++ = s2j++;
+
+	
+
+	ft_strlcat
+	return (str)
+}
+
+
+
