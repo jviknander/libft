ff235dff5239265eb5487ce3d7b01bca7aa286be
Julia claudia De melo viknander <jde-melo@c1r15s10.42lisboa.com>
2021-11-12 19:17:10 +0000

    ok now its almost done :angry:

---
+++ ft_lstadd_back.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jde-melo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/12 18:35:40 by jde-melo          #+#    #+#             */
+/*   Updated: 2021/11/12 19:16:23 by jde-melo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (!new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	t_list *temp;
+	temp = ft_lstlast(*lst);
+	temp->next = new;
+	new->next = NULL;
+	return ;
+}
