93bd217a0160dbf6cf3ace55f809e705ac31cb21
Julia claudia De melo viknander <jde-melo@c1r19s6.42lisboa.com>
2021-11-13 19:01:25 +0000

    loading... 98%

---
+++ ft_lstclear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jde-melo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/13 15:52:54 by jde-melo          #+#    #+#             */
+/*   Updated: 2021/11/13 19:00:36 by jde-melo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*temp;
+
+	while (*lst)
+	{
+		temp = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = temp;
+	}
+	*lst = NULL;
+	return ;
+}
